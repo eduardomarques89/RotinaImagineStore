a601ed3fb0f8fca3a5501b9382c50a500c85944f
W7 <wi7h.solutions@gmail.com>
2024-08-02 17:16:20 -0300

    routine

--- RotinaImagineStore/Program.cs
+++ RotinaImagineStore/Program.cs
@@ -31,7 +31,7 @@ namespace RotinaImagineStore
 
             while (page <= maxPage)
             {
-                var request = new RestRequest($"/cashless_facts?access_token=04PJ5nF3VnLIfNLJRbqmZkEMhU2VNCClOjPoTPCI&start_date=2024-07-25&end_date=2024-07-31&page={page}&per_page={perPage}");
+                var request = new RestRequest($"/cashless_facts?access_token=04PJ5nF3VnLIfNLJRbqmZkEMhU2VNCClOjPoTPCI&start_date=2024-08-01&end_date=2024-08-05&page={page}&per_page={perPage}");
 
                 request.AddHeader("Accept", "application/json");
 
